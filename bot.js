1da6494f44972740767e60e6a4efe0750d60ce00
ほと <mail@hoto.me>
2016-08-29 00:21:33 +0900

    redis

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 const RtmClient = require('@slack/client').RtmClient;
+const RedisStorage = require('./lib/RedisStorage');
 
 const RTM = new RtmClient( process.env.SLACK_TOKEN );
+const Storage = new RedisStorage( process.env.REDIS_URL );
 
 RTM.start();
