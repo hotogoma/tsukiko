414ce7a06b4722b8ee2f0e6b30aa0676d78cd583
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2016-09-10 10:29:30 +0900

    general.js

--- bot.js
+++ bot.js
@@ -9,10 +9,21 @@ const bot = new SlackBot({
 });
 
 
-bot.hear(/hoge/, (msg) => {
-  msg.send('foo');
-  msg.reply('bar');
-});
+bot.respond(/ぺろぺろ/i, (msg) => msg.reply('まったくどうしようもない変態さんですね'));
+bot.hear(/月子ちゃんぺろぺろ/i, (msg) => msg.reply('まったくどうしようもない変態さんですね'));
+bot.hear(/^月子ちゃん$/i, (msg) => msg.reply('なんですか'));
+bot.hear(/(疲|つか)れた/i, (msg) => msg.reply('おつかれさまです先輩'));
+
+bot.respond(/(花金|華金|はなきん)$/i, (msg) => msg.send('花金だーワッショーイ！テンションAGEAGEマック'));
+
+bot.respond(/say (.+)/i, (msg) => bot.send(msg.match[1]));
+
+bot.respond(/PING$/i, (msg) => msg.reply('PONG!'));
+
+// 日本語版 ping (http://pasero.net/~mako/blog/s/679)
+const pingRegex = /(((い|ゐ|居)(て?))(?!り)|(お|を|居)|((い|居)(て?)は)(?!ま))((る|ん(?=の))|((り?)ます)(?!ん))((の?ん?)(です)?|(んだ)(?!か))?(か(い?な?|よ|ね)?|(よ?)(ね|な))?\s?(\?|？)/i;
+bot.respond(pingRegex, (msg) => msg.send('はい'));
+
 
 bot.respond(/天気/, (msg) => {
   var title = '明日の天気は 雪 です';
