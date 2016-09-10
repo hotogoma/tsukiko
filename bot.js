517842d4416e18af6fe1ca628a99f8b8acbaedbd
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2016-09-10 11:33:36 +0900

    image.js

--- bot.js
+++ bot.js
@@ -29,6 +29,28 @@ bot.respond(pingRegex, (msg) => msg.send('はい'));
 const SafeEval = require('./lib/SafeEval');
 bot.respond(/eval (.+)$/i, (msg) => bot.send(SafeEval(msg.match[1])));
 
+// image.js
+Array.prototype.pickRandom = function() {
+  return this[ Math.floor( Math.random() * this.length ) ];
+};
+
+const GoogleImages = require('google-images');
+
+const GOOGLE_CSE_ID  = process.env.GOOGLE_CSE_ID;
+const GOOGLE_CSE_KEY = process.env.GOOGLE_CSE_KEY;
+
+if ( GOOGLE_CSE_ID && GOOGLE_CSE_KEY ) {
+  const client = GoogleImages(GOOGLE_CSE_ID, GOOGLE_CSE_KEY);
+
+  bot.respond(/image (.+)$/i, (msg) => {
+    client.search(msg.match[1]).then((images) => {
+      if ( images.length > 0 ) msg.send( images.pickRandom().url );
+    }).catch((e) => console.error(e))
+  });
+}
+
+
+
 bot.respond(/天気/, (msg) => {
   var title = '明日の天気は 雪 です';
   var attachment = {
