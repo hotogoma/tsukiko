52994f83a3389957cacfba94e29081948297d469
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2016-09-11 18:57:46 +0900

    irkit.js

--- bot.js
+++ bot.js
@@ -50,6 +50,31 @@ if ( GOOGLE_CSE_ID && GOOGLE_CSE_KEY ) {
 }
 
 
+const IRKit = require('irkit');
+const signals = require('../config/irkit.json');
+const irkit = new IRKit();
+
+if ( ! irkit.available() ) { return; }
+
+var patterns = [
+  { regex: /テレビ(点|つ)けて/i,             signal: signals.tv.power,       message: 'テレビをつけましたよ'   },
+  { regex: /テレビ(消|け)して/i,             signal: signals.tv.power,       message: 'テレビを消しましたよ'   },
+  { regex: /エアコン(点|つ)けて/i,           signal: signals.aircon.on,      message: 'エアコンをつけましたよ' },
+  { regex: /エアコン(消|け)して/i,           signal: signals.aircon.off,     message: 'エアコンを消しましたよ' },
+  { regex: /Apple\s?TV/i,                    signal: signals.appletv.select, message: 'AppleTV をつけましたよ' },
+  { regex: /(電気|照明|ライト)(点|つ)けて/i, signal: signals.light.on,       message: '照明をつけましたよ'     },
+  { regex: /(電気|照明|ライト)(消|け)して/i, signal: signals.light.off,      message: '照明を消しましたよ'     },
+];
+
+patterns.forEach((pattern) => {
+  robot.respond(pattern.regex, (msg) => {
+    irkit.send( pattern.signal )
+      .then(() => msg.send( pattern.message ))
+      .catch((errMsg) => msg.send( errorMsg ));
+  });
+});
+
+
 
 bot.respond(/天気/, (msg) => {
   var title = '明日の天気は 雪 です';
