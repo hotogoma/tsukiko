6bdfcbc7c40d52b601cb3e12249264313083e9e4
Yuki Ishikawa <mail@hoto.me>
2016-09-21 22:52:21 +0900

    extends EventEmitter

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 const SlackBot = require('./lib/SlackBot');
 const RedisStorage = require('./lib/RedisStorage');
+const JobList = require('./lib/JobList');
+const express = require('express');
 const KuromojiMiddleware = require('./lib/KuromojiMiddleware');
 const scripts = require('./scripts');
 
@@ -10,10 +12,20 @@ Array.prototype.pickRandom = function() {
 const bot = new SlackBot({
   token: process.env.SLACK_TOKEN,
   default_channel: { id: process.env.SLACK_DEFAULT_CHANNEL_ID },
-  storage: new RedisStorage( process.env.REDIS_URL ),
-  middlewares: [ new KuromojiMiddleware() ],
   debug: process.env.NODE_ENV !== 'production',
 });
+
+bot.storage = new RedisStorage( process.env.REDIS_URL );
+bot.jobs = new JobList();
+bot.http = express();
+
+bot.on(SlackBot.EVENTS.CONNECTION.OPENED, () => {
+  bot.jobs.startAll();
+  bot.http.listen( process.env.HTTP_PORT || 80 );
+});
+
+const kuromoji = new KuromojiMiddleware();
+bot.on(SlackBot.EVENTS.MESSAGE.RECEIVED, kuromoji.onReceive.bind(kuromoji));
 
 scripts.forEach((script) => script(bot));
 
