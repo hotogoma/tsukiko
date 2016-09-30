bfcd388d76fccde88c428d39edfd743d68c22a33
ほと <mail@hoto.me>
2016-09-30 23:40:14 +0900

    Bot Framework を切り出し

--- bot.js
+++ bot.js
@@ -1,9 +1,7 @@
-const SlackBot = require('./lib/SlackBot');
-const RedisStorage = require('./lib/RedisStorage');
-const JobList = require('./lib/JobList');
+const SlackBot = require('slackbot');
+const utils = require('slackbot-utils');
 const express = require('express');
 const bodyParser = require('body-parser');
-const KuromojiMiddleware = require('./lib/KuromojiMiddleware');
 const members = require('./configs/members');
 const scripts = require('./scripts');
 
@@ -15,14 +13,14 @@ const bot = new SlackBot(process.env.SLACK_TOKEN, {
   default_channel: process.env.SLACK_DEFAULT_CHANNEL,
 });
 
-bot.storage = new RedisStorage( process.env.REDIS_URL );
+bot.storage = new utils.RedisStorage( process.env.REDIS_URL );
 
-bot.jobs = new JobList();
+bot.jobs = new utils.JobList();
 
 bot.http = express();
 bot.http.use(bodyParser.json());
 
-const kuromoji = new KuromojiMiddleware();
+const kuromoji = new utils.KuromojiMiddleware();
 bot.on(SlackBot.EVENTS.MESSAGE.RECEIVED, kuromoji.onReceive.bind(kuromoji));
 
 scripts.forEach((script) => script(bot));
