18bcd03a68211e35c91296e4de017a90d2c6b0ae
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2016-10-01 17:44:45 +0900

    ﾀｲｼｬ機能を改修

--- scripts/taisha.js
+++ scripts/taisha.js
@@ -4,7 +4,9 @@ module.exports = (bot) => {
 
   bot.http.post('/taisha', (req, res) => {
     if ( token && req.query.token !== token ) return res.send(401);
-    bot.send(`【${req.body.username}】ﾀｲｼｬ!!!`, () => res.send(200));
+    const user = bot.data.members.filter((u) => u.name === req.body.name)[0];
+    if ( ! user ) return res.send(404);
+    bot.send(`【${user.real_name}】ﾀｲｼｬ!!!`, () => res.send(200));
   });
 
 };
