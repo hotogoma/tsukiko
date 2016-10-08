ca1a93348fda2e9758aa937eee549f29d7634e04
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2016-10-08 16:33:32 +0900

    ﾀｲｼｬ通知しない条件を追加

--- scripts/taisha.js
+++ scripts/taisha.js
@@ -3,6 +3,9 @@ const help = {
   description: '退社したら通知',
 };
 
+const Shukjitz = require('shukjitz');
+const shukjitz = new Shukjitz();
+
 const token = process.env.HTTP_ACCESS_TOKEN;
 
 module.exports = (bot) => {
@@ -11,6 +14,15 @@ module.exports = (bot) => {
     if ( token && req.query.token !== token ) return res.send(401);
     const user = bot.data.members.filter((u) => u.name === req.body.name)[0];
     if ( ! user ) return res.send(404);
+
+    const date = new Date();
+    // 土日は通知しない
+    if ( date.getDay() % 6 === 0 ) return;
+    // 祝日は通知しない
+    if ( shukjitz.checkSync(date) ) return;
+    // 16時以前は通知しない
+    if ( date.getHours() < 16 ) return;
+
     bot.send(`【${user.real_name}】ﾀｲｼｬ!!!`, () => res.send(200));
   });
 
