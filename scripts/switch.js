4d5d2131643c423b6d44c2b83baa4b82e7cb26b2
ほと <mail@hoto.me>
2017-03-06 17:47:06 +0900

    switch

---
+++ scripts/switch.js
@@ -0,0 +1,19 @@
+const help = {
+  title: 'Switch',
+  description: '日が沈んだらスイッチやっていい',
+};
+
+const token = process.env.HTTP_ACCESS_TOKEN;
+
+module.exports = (bot) => {
+
+  bot.http.post('/switch', (req, res) => {
+    if ( token && req.query.token !== token ) return res.send(401);
+    console.log(req.body);
+    if ( req.body.status === 'sunset') bot.send('スイッチ オン!!');
+    res.send(200);
+  });
+
+};
+
+module.exports.help = help;
