60149f3842ab863403fddb094f64fa070ffc53d1
hoto17296 <mail@hoto.me>
2017-03-24 15:08:56 +0900

    add test dir

---
+++ test/scripts/t_wada.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const bot = require('../..');
+
+describe('t_wada', () => {
+  context('when receive "テスト書いてない"', () => {
+    it('should send t_wada message', () => {
+      // TODO
+    });
+  });
+});
